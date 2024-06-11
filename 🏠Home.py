a163ff91f5f5b2dd53e7c0a819e5faa0e7e8b778
Mustafa Akbas <gyrfalcondynamics@gmail.com>
2024-06-11 18:19:00 +0100

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -1,23 +1,46 @@
 import streamlit as st
-import yfinance as yf
-import datetime
+import pandas as pd
+import requests
 
-st.header("My Finance App")
+st.header("My Bike Price Prediction App")
 
-symbol = st.sidebar.text_input("Enter a stock symbol", value="AAPL")
+gabel_federweg_mm = st.sidebar.slider("Please provide the travel length of the fork in millimeters.", min_value=30, max_value=200)
+akkukapazität_wh = st.sidebar.slider("What is the watt-hour capacity of your electric bike's battery?", min_value=248, max_value=850)
+rahmenmaterial= st.sidebar.selectbox("What material was used for the frame of your bicycle (e.g., aluminum, carbon, steel)?", ('Aluminium', 'Carbon', 'Aluminium-Carbon', 'Diamant', 'Aluminium-Stahl'))
+gänge=st.sidebar.slider("Please specify the number of gears on your bicycle.",min_value=1, max_value=30)
+#bremse_vorne=st.sidebar.selectbox("Which brand of braking system located at the front of your bicycle (e.g., MAGURA HS-11 , Shimano MT-200).", ('Shimano MT-200'))
+#schaltwerk=st.sidebar.selectbox("Which rear derailleur is installed on your bicycle? (e.g., Shimano Deore, SRAM GX)", ('Shimano ', 'Shimano Deore))
+kategorie=st.sidebar.selectbox("To which category does your bicycle belong? (e.g., mountain bike, road bike, electric bike)", ('Trekking', 'City', 'MTB_Hardtail', 'MTB_Fully'))                                                                                                          
+hersteller=st.sidebar.selectbox("Who is the manufacturer of your bicycle?", (  'Kalkhoff',
+                                                                                'CUBE',
+                                                                                'Haibike',
+                                                                                'Hercules',
+                                                                                'Winora',
+                                                                                'SCOTT',
+                                                                                'corratec',
+                                                                                'Diamant',
+                                                                                'GHOST',
+                                                                                'Specialized',
+                                                                                'Cannondale',
+                                                                                'Canyon'))
 
-mid_value = st.sidebar.slider('Slide me', min_value=0, max_value=100)
+my_dict = {
+    "gabel_federweg_mm": gabel_federweg_mm,
+    "akkukapazität_wh": akkukapazität_wh,
+    "rahmenmaterial": rahmenmaterial,
+    'gänge': gänge,
+    "kategorie": kategorie,
+    "hersteller": hersteller    
+}
+
+df = pd.DataFrame.from_dict([my_dict])
 
 
-st.title(f"Stock price of {symbol}")
+st.header("The configuration of your e-bike is below!")
+st.table(df)
 
-start_date = st.sidebar.date_input("Start date", value=datetime.date(2023, 1, 1))
-end_date = st.sidebar.date_input("Start date", value=datetime.date(2024, 1, 1))
+st.subheader("Press predict if configuration is okay!!!")
 
-df = yf.download(symbol, start=start_date, end=end_date)
-
-st.line_chart(df["Close"])
-
-st.subheader("Volume")
-
-st.write(df)
+if st.button("Predict"):
+    response = requests.get(f"https://demo-fastapi-jzlx.onrender.com/prediction/{gabel_federweg_mm}/{akkukapazität_wh}/{rahmenmaterial}/{gänge}/{kategorie}/{hersteller}").json()
+    st.success("The estimated price of your e-bike is {}{}. ".format(response["unit"],int(response["prediction"])))
